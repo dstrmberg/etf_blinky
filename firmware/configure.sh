65d77bba54247628c9f718ac385ea8ea565c4a15
Jonathan Sönnerup <nooxet@gmail.com>
2024-09-22 00:25:02 +0200

    Added test config

--- firmware/configure.sh
+++ firmware/configure.sh
@@ -15,3 +15,10 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -G Ninja \
     -B build/release
+
+# tests
+cmake \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DBUILD_TESTS=ON \
+    -G Ninja \
+    -B build/tests-host
